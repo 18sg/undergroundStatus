69fde6aff237663cd88f4827ff5c099c94de7cd8
James Sandford <jamesmsandford@googlemail.com>
2013-07-07 19:16:43 +0100

    Should now actually be fixed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='undergroundStatus',
     version='0.1',
     author='James Sandford',
-    packages=['undergroundStatus'],
+    py_modules=['undergroundStatus'],
     url='https://github.com/18sg/undergroundStatus',
     description='A small python module to grab line statuses from the London Underground',
     
