41df1a9a66e0f8f4a7ddbce4af0f90c2872725c9
James Sandford <jamesmsandford@googlemail.com>
2013-07-07 19:12:25 +0100

    Fixed?

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='undergroundStatus',
     version='0.1',
     author='James Sandford',
-    scripts=[],
+    packages=['undergroundStatus'],
     url='https://github.com/18sg/undergroundStatus',
     description='A small python module to grab line statuses from the London Underground',
     
